060634f409c354192fddc1dd45bebd97546a5749
RicardoIslas <ricardo_islas19@hotmail.com>
2025-07-18 16:49:46 -0600

    commit inicial

---
+++ ExtraMacOS/RicardoIslasExtraMacOS/RicardoIslasExtraMacOSTests/RicardoIslasExtraMacOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RicardoIslasExtraMacOSTests.swift
+//  RicardoIslasExtraMacOSTests
+//
+//  Created by Zap Islas on 16/07/25.
+//
+
+import Testing
+@testable import RicardoIslasExtraMacOS
+
+struct RicardoIslasExtraMacOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
